53c6950b0d95353c96147b76e5e49663175faf41
Jiayu Hu <adamruisdael@gmail.com>
2015-11-22 16:48:39 +0100

    Encryption version 1.01: Removed 1 unused "import".

--- src/main/java/org/tasks/activities/EncryptKeyActivity.java
+++ src/main/java/org/tasks/activities/EncryptKeyActivity.java
@@ -9,8 +9,6 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.todoroo.astrid.security.Encryption;
-
 import javax.inject.Inject;
 import org.tasks.R;
 import org.tasks.injection.InjectingAppCompatActivity;
